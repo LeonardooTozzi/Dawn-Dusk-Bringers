6bdec7c9188a92c9bd2d6dbd9e99f5b2c1f8789b
Tozzi <TozzileoDev@gmail.com>
2024-10-13 21:53:46 -0300

    initial commit

---
+++ src/main/java/com/dawnbringers/DawnBringersApplication.java
@@ -0,0 +1,13 @@
+package com.dawnbringers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawnBringersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DawnBringersApplication.class, args);
+    }
+
+}
